50ccb1aae614c86c9cf01b190dd0690c0e82a501
gitsrc <167502426+BlockCraftsman@users.noreply.github.com>
2024-05-30 05:35:17 -0400

    feat(dashboard):support curd on chain
    Signed-off-by: gitsrc <167502426+BlockCraftsman@users.noreply.github.com>

--- vue/src/views/DataView.vue
+++ vue/src/views/DataView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Uncomment the following component to add a form for a `modelName` -->
-    <!-- <IgntCrud store-name="OrgRepoModule" item-name="modelName" /> -->
+    <IgntCrud store-name="BlockcraftsmanWarmstoragechainWarmstoragechain" item-name="Resource" />
   </div>
 </template>
 <script setup lang="ts">
