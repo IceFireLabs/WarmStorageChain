391ef58e37b751fd200b1df0d6c2191b1f7d4ac7
gitsrc <167502426+BlockCraftsman@users.noreply.github.com>
2024-05-31 00:53:49 +0800

    feat(cid):add memory kv to support cid hot layer
    Signed-off-by: gitsrc <167502426+BlockCraftsman@users.noreply.github.com>

--- vue/src/env.ts
+++ vue/src/env.ts
@@ -1,5 +1,5 @@
-const apiURL = import.meta.env.VITE_API_COSMOS ?? "http://64.112.124.220:1317";
-const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "http://64.112.124.220:26657";
+const apiURL = import.meta.env.VITE_API_COSMOS ?? "http://127.0.0.1:1317";
+const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "http://127.0.0.1:26657";
 const prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? "cosmos";
 
 export const env = {
--- x/warmstoragechain/keeper/resource.go
+++ x/warmstoragechain/keeper/resource.go
@@ -1,14 +1,54 @@
 package keeper
 
 import (
+	"errors"
+	"log"
+
+	"github.com/BlockCraftsman/WarmStorageChain/pkg/ipfs"
 	"github.com/BlockCraftsman/WarmStorageChain/x/warmstoragechain/types"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/patrickmn/go-cache"
 )
+
+var MemKV *cache.Cache
+
+func init() {
+	MemKV = cache.New(cache.NoExpiration, cache.NoExpiration)
+
+}
 
 // SetResource set a specific resource in the store from its index
 func (k Keeper) SetResource(ctx sdk.Context, resource types.Resource) {
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.ResourceKeyPrefix))
+
+	// Filecoin CID retrieval
+	CID := resource.Cid
+	dataS, err := ipfs.GetDATAFromIPFSCID(ipfs.GetReuseIPFSClient(), CID)
+
+	if err != nil {
+		log.Println("SetResource Error", err)
+		return
+	}
+
+	//Just store the first CID file
+	if len(dataS) < 1 {
+		log.Println(errors.New("data of CID is nil"))
+		return
+	}
+
+	CIDdata := dataS[0]
+
+	// err = KVDB.Put([]byte(resource.Index), CIDdata, nil)
+
+	// if err != nil {
+	// 	log.Println("SetResource NoSQLDB.Set Fail ", err)
+	// 	return
+	// }
+	MemKV.Set(resource.Index, CIDdata, cache.NoExpiration)
+
+	log.Println("SetResource NoSQLDB.Set Success", "Key", resource.Index, "Value length", len(CIDdata))
+
 	b := k.cdc.MustMarshal(&resource)
 	store.Set(types.ResourceKey(
 		resource.Index,
@@ -53,6 +93,7 @@ func (k Keeper) GetAllResource(ctx sdk.Context) (list []types.Resource) {
 
 	defer iterator.Close()
 
+	log.Println("342134234")
 	for ; iterator.Valid(); iterator.Next() {
 		var val types.Resource
 		k.cdc.MustUnmarshal(iterator.Value(), &val)
